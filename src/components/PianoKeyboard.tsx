9948b8328fcf06b909bfd50aae4be249b847c9db
Joel Plowright <joelyjoel@protonmail.com>
2020-12-22 14:51:13 +0000

    improved PianoKeyboard

--- src/components/PianoKeyboard.tsx
+++ src/components/PianoKeyboard.tsx
@@ -4,26 +4,40 @@ import {FunctionComponent} from 'react';
 import './PianoKeyboard.sass';
 
 const keyNames = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
+const defaultHotKeys = ['a','w','s','e','d','f','t','g','y','h','u','j','k','o','l', 'p', ';'];
 
 export const PianoKeyboard: FunctionComponent<{
   numberOfKeys?: number;
   octave?: number;
-}> = ({numberOfKeys=20, octave=4}) => {
+  hotKeys?: string[];
+  onNote?: (e:{
+    pitchNumber: number;
+    p: number;
+    pitchName: string;
+  }) => void
+}> = ({numberOfKeys=92, octave=1, hotKeys=defaultHotKeys, onNote}) => {
 
-  let whiteNotes = []
-  let blackNotes = []
+  const whiteNotes = []
+  const blackNotes = []
   for(let i=0; i < numberOfKeys; ++i) {
-    let keyName = keyNames[i % 12]
-    if(/b$/.test(keyName))
-      // black
-      blackNotes.push(
-        <button className={keyName} />
-      )
+    const keyName = keyNames[i % 12]
+    const black = /[b#]$/.test(keyName)
+    const hotKey = hotKeys[i] || null
+    const pitch = octave * 12 + i
+    const handlePress = () => {
+      if(onNote)
+        onNote({
+          p: pitch,
+          pitchNumber: pitch,
+          pitchName: keyName,
+        })
+    }
+
+    const btn = <button onMouseDown={handlePress} key={i} className={keyName}>{hotKey || ' '}</button>
+    if(black)
+      blackNotes.push(btn)
     else
-      // white
-      whiteNotes.push(
-        <button className={keyName} />
-      )
+      whiteNotes.push(btn)
   }
 
   return <div className='PianoKeyboard'>
